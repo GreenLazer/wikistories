8c2f024629a40854f62f55c79227e16bd67a6f05
GreenLazer <Simone@Namer.home>
2018-01-06 16:56:13 -0500

    gets wiki2 all ps

--- wikistories2.py
+++ wikistories2.py
@@ -1,4 +1,3 @@
-
 import random
 import math
 import nltk
@@ -15,31 +14,40 @@ def randonum(b, e):
     return int(math.floor(random.uniform(b,e)))
 
 page_soup = BeautifulSoup(requests.get(my_url).text, "html.parser")
-tablee = page_soup.find("table", {'class':'sortable'})
-links= tablee.findAll("a")
+tablee = page_soup.findAll("i")
 
-for link in links:
-    if "/wiki/" in link['href']:
-        my_url = wiks + link['href']
-        page_soup = BeautifulSoup(requests.get(my_url).text, "html.parser")
-        yo= page_soup.findAll("p")
-        for i in range(6,8):
-            sens=str(yo[i]).split(',')
-            text=[]
-            for sen in sens:
-                try:
-                    print(sen)
-                    text.append(sen)
-                except:
-                    print('fail1')
-        texts = texts + text[randonum(0,len(text)-1)]
+for row in tablee:
+    try:
+        links.append(row.find('a')['href'])
+    except:
+        fail = True
+
+for link in links[20:494]:
+    my_url = wiks + link
+    page_soup = BeautifulSoup(requests.get(my_url).text, "html.parser")
+
+    plot = page_soup.findAll('h2')[1]
+    endplot = page_soup.findAll('h2')[2]
+    current = plot.next_sibling
+    while current != endplot:
+        paras.append(current)
+        current = current.next_sibling
+
+    for para in paras:
+        sens=str(para).split(',')
+        text=[]
+        for sen in sens:
+            try:
+                print(sen)
+                text.append(sen)
+            except:
+                print('fail1')
+    texts = texts + text[randonum(0,len(text)-1)]
             # num=randonum(6,10)
             # print(yo[num])
             # sens=str(yo[num]).split('.')
             # num=randonum(0,3)
             # text.append(sens[num])
-    else:
-        print('fail2')
         # link= links[math.floor(random.uniform(0,10))]
         # print(link['href'])
         # new_link=wiks + link['href']
