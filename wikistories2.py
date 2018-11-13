d009a45498665bfadaaeabcae50979c8eb44df9f
GreenLazer <sarfs48@gmail.com>
2018-11-13 19:24:14 +0200

    export all texts to alltext.txt

--- wikistories2.py
+++ wikistories2.py
@@ -25,8 +25,8 @@ for row in tablee:
         links.append(row.find('a')['href'])
     except:
         fail = True
-
-for link in links[20:494]:
+#20-494 are valid-ish
+for link in links[20:490]:
     print(linknum)
     linknum += 1
     my_url = wiks + link
@@ -38,31 +38,8 @@ for link in links[20:494]:
         while current != endplot:
             paras.append(current)
             current = current.next_sibling
-
-        for para in paras:
-            sens=str(para).split(',')
-            text=[]
-            text.append(sens[randonum(0,len(sens)-1)])
-            print("new para")
-        texts = texts + text[randonum(0,len(text)-1)]
     except:
         print('fail')
-            # num=randonum(6,10)
-            # print(yo[num])
-            # sens=str(yo[num]).split('.')
-            # num=randonum(0,3)
-            # text.append(sens[num])
-        # link= links[math.floor(random.uniform(0,10))]
-        # print(link['href'])
-        # new_link=wiks + link['href']
-        #
-        # while new_link != wiks + link['href']:
-        #     link= links[math.floor(random.uniform(0,10))]
-        #     new_link=wiks + link['href']
-        #
-        # links.append(new_link)
-        # print(new_link)
-        # my_url=new_link
-
-
-print(texts)
+with open('alltext.txt', 'w') as f:
+    for i in paras:
+        f.write(str(i))
