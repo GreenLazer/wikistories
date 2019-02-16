dd8d52ac02558c5c8d6e388b3b7d0f70e9a6f574
GreenLazer <sarfs48@gmail.com>
2019-02-16 23:45:38 +0200

    Create text Version 1: Delete every word between every other proper noun

--- DownloadScramble.py
+++ DownloadScramble.py
@@ -1,10 +1,30 @@
 import nltk
+from nltk import pos_tag, word_tokenize
+sents = ""
 
 with open('text.txt', 'r') as f:
     textuncoded = f.read()
     f.close()
 
 text = repr(textuncoded.decode('unicode-escape'))
-tokens = nltk.word_tokenize(text)
-tagged = nltk.pos_tag(tokens)
-print tagged
+words = nltk.word_tokenize(text)
+PoS = pos_tag(words)
+
+word = 0
+while word < len(words):
+    wordT = PoS[word] #assign wordT part of speech connected to current word
+    if wordT[1] == 'NNP': #if part of speech is proper noun add word to sents, moce to next word, and start scanning
+        sents += words[word] + " "
+        word += 1
+        wordT = PoS[word]
+        while wordT[1] != 'NNP' and word < len(words): #loops through words until find next proper noun or finishes all words
+            print words[word]
+            wordT = PoS[word]
+            word+= 1
+    else:
+        sents += words[word] + " "
+        word += 1
+
+with open('scrambledtext.txt', 'w') as f:
+    for i in sents:
+        f.write(str(i))
