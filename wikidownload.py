ea6242c1504fd110072ae33331cc6368a2c52d8f
GreenLazer <sarfs48@gmail.com>
2019-04-29 17:07:10 +0300

    Redesign Scraper to get cleaner results

--- wikidownload.py
+++ wikidownload.py
@@ -2,35 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 my_url='https://en.wikipedia.org/wiki/List_of_highest-grossing_films'
 wiks='https://en.wikipedia.org'
-fail = True
-links = []
+hrefs = []
 paras = []
-linknum = 23
+hrefnum = 0
 
 page_soup = BeautifulSoup(requests.get(my_url).text, "html.parser")
-tablee = page_soup.findAll("i")
+links = page_soup.findAll("a")
 
-for row in tablee:
-    try:
-        links.append(row.find('a')['href'])
-    except:
-        fail = True
-for link in links[linknum:290]:
-    print(linknum)
-    linknum += 1
-    my_url = wiks + link
+for link in links:
+    if link.parent.name == 'i':
+        if link.parent.parent.name == "td" or link.parent.parent.name == "th":
+            page_name = link.get('href')
+            if not any(x in page_name for x in ("franchise","series","trilogy","films","filmography","Anthology", "Extended_Universe", "Marvel_Studios")):
+                hrefs.append(page_name)
+
+for href in hrefs:
+    print(hrefnum)
+    hrefnum += 1
+    my_url = wiks + href
     page_soup = BeautifulSoup(requests.get(my_url).text, "html.parser")
-    try:
-        plot = page_soup.findAll('h2')[1]
-        endplot = page_soup.findAll('h2')[2]
-        current = plot.next_sibling
-        while current != endplot:
-            if current.name == 'p':
-                print current.name
-                paras.append(current)
-            current = current.next_sibling
-    except:
-        print('fail')
+    plotstart = page_soup.findAll('h2')[1]
+    plotend = page_soup.findAll('h2')[2]
+    current = plotstart.next_sibling
+    while current != plotend:
+        if current.name == 'p':
+            paras.append(current)
+        current = current.next_sibling
 with open('alltext.html', 'w') as f:
     for i in paras:
         f.write(str(i))
+#313,464 words 
